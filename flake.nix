7d830b5abf4c71be9d1609eccee0232191bbd02f
Mariell Hoversholm <mariell@mardroemmar.dev>
2024-07-05 16:53:09 +0200

    feat: init a flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "mari's neovim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages = {
+          hello = pkgs.hello;
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
